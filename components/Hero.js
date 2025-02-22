ba80662a26e95c07ce0007bbc6e96551493b170c
monaheng101 <ntaimonaheng@gmail.com>
2025-02-22 13:14:38 +0200

    data update

--- components/Hero.js
+++ components/Hero.js
@@ -9,8 +9,8 @@ const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] })
 export default function Hero() {
   return (
     <div className='py-4 md:py-10 flex flex-col gap-8  sm:gap-10 '>
-      <h1 className={'text-5xl sm:text-text-6xl md:text-7xl text-center ' + fugaz.className}><span className='textGradient'>Bible Inspiration</span> tracks your <span className='textGradient'>daily</span> mood!</h1>
-      <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Select your current mood to learn a recomended <span className='font-semibold'>Bible Verse for your inspiration.</span></p>
+      <h1 className={'text-5xl sm:text-text-6xl md:text-7xl text-center ' + fugaz.className}><span className='textGradient'>Bible Inspiration</span> helps your <span className='textGradient'>daily</span> mood!</h1>
+      <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Tell us <span className='font-semibold'>how you feel</span> to learn a <span className='font-semibold'>Bible Verse</span> that goes with that mood.</p>
       <div className='w-fit mx-auto'>
         <Link href={'/dashboard'}>
           <Button text="Continue"/>
